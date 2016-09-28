676abf1a095934f7b6a2043766e10fdf8e219dd3
Maurits van Rees <maurits@vanrees.org>
2016-09-28 02:51:25 +0200

    Back to development: 1.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='experimental.publishtraverse',
-    version='1.0',
+    version='1.0.1.dev0',
     description="Experimental PublishTraverse implementation.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
